c8bf5f3065fab7c1f85980b342c3fb07e05915aa
languanzhao <1736626696@qq.com>
2020-02-20 22:51:42 +0800

    uni-app

---
+++ pages/user/user.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="content">
+      {{title}}
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '我的'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
