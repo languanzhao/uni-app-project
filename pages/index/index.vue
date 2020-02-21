f85339ff73d2fb2b88f3556c493426189ff3e85f
languanzhao <1736626696@qq.com>
2020-02-21 19:44:58 +0800

    2020-02-21

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,39 +1,53 @@
 <template>
-	<view class="content">
-		<view>
-            <text class="title">{{title}}</text>
-        </view>
+	<view class="index">
+		<swiper class="swiper" :previous-margin="30" circular :next-margin="30" interval="3000" autoplay @change="swiperChange">
+			<swiper-item class="swiper-item" v-for="(item, index) of list" :key="index">
+				<image :class="['image', swiperIndex === index ? 'active' : '']" :src="item" mode=""></image>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: '首页'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
+export default {
+	data() {
+		return {
+			swiperIndex: 0,
+			list: [
+				'http://img2.imgtn.bdimg.com/it/u=2249146272,3803357286&fm=26&gp=0.jpg',
+				'http://img3.imgtn.bdimg.com/it/u=2549165567,3171022610&fm=26&gp=0.jpg',
+				'http://img2.imgtn.bdimg.com/it/u=1890128306,362626824&fm=26&gp=0.jpg'
+			]
+		};
+	},
+	onLoad() {},
+	methods: {
+		swiperChange(e) {
+			this.swiperIndex = e.detail.current;
 		}
 	}
+};
 </script>
 
-<style>
-	.content {
-		text-align: center;
-		height: 400upx;
+<style lang="scss">
+.index {
+	.swiper {
+		width: 100%;
+		height: 300upx;
+		.swiper-item {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			.image {
+				width: 100%;
+				height: 260upx;
+				margin: 0 20upx;
+			}
+			.active {
+				transform: scale(1.14);
+				transition: all 0.2s ease-in 0.2s;
+			}
+		}
 	}
-    .logo{
-        height: 200upx;
-        width: 200upx;
-        margin-top: 200upx;
-    }
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
+}
 </style>
--- unpackage/dist/dev/app-plus/app-view.js
+++ unpackage/dist/dev/app-plus/app-view.js
@@ -1061,9 +1061,20 @@ function gz$gwx_1(){
 if( __WXML_GLOBAL__.ops_cached.$gwx_1)return __WXML_GLOBAL__.ops_cached.$gwx_1
 __WXML_GLOBAL__.ops_cached.$gwx_1=[];
 (function(z){var a=11;function Z(ops){z.push(ops)}
-Z([3,'content'])
-Z([3,'title'])
-Z([a,[[7],[3,'title']]])
+Z([3,'index'])
+Z([3,'__e'])
+Z([3,'swiper'])
+Z([[4],[[5],[[4],[[5],[[5],[1,'change']],[[4],[[5],[[4],[[5],[[5],[1,'swiperChange']],[[4],[[5],[1,'$event']]]]]]]]]]])
+Z([3,'3000'])
+Z([1,30])
+Z(z[5])
+Z(z[0])
+Z([3,'item'])
+Z([[7],[3,'list']])
+Z(z[0])
+Z([3,'swiper-item'])
+Z([[4],[[5],[[5],[1,'image']],[[2,'?:'],[[2,'==='],[[7],[3,'swiperIndex']],[[7],[3,'index']]],[1,'active'],[1,'']]]])
+Z([[7],[3,'item']])
 })(__WXML_GLOBAL__.ops_cached.$gwx_1);return __WXML_GLOBAL__.ops_cached.$gwx_1
 }
 function gz$gwx_2(){
@@ -1083,12 +1094,19 @@ var m0=function(e,s,r,gg){
 var z=gz$gwx_1()
 var oB=_n('view')
 _rz(z,oB,'class',0,e,s,gg)
-var xC=_n('view')
-var oD=_n('text')
-_rz(z,oD,'class',1,e,s,gg)
-var fE=_oz(z,2,e,s,gg)
-_(oD,fE)
+var xC=_mz(z,'swiper',['autoplay',-1,'circular',-1,'bindchange',1,'class',1,'data-event-opts',2,'interval',3,'nextMargin',4,'previousMargin',5],[],e,s,gg)
+var oD=_v()
 _(xC,oD)
+var fE=function(hG,cF,oH,gg){
+var oJ=_n('swiper-item')
+_rz(z,oJ,'class',11,hG,cF,gg)
+var lK=_mz(z,'image',['mode',-1,'class',12,'src',1],[],hG,cF,gg)
+_(oJ,lK)
+_(oH,oJ)
+return oH
+}
+oD.wxXCkey=2
+_2z(z,9,fE,e,s,gg,oD,'item','index','index')
 _(oB,xC)
 _(r,oB)
 return r
@@ -1097,11 +1115,11 @@ e_[x[0]]={f:m0,j:[],i:[],ti:[],ic:[]}
 d_[x[1]]={}
 var m1=function(e,s,r,gg){
 var z=gz$gwx_2()
-var hG=_n('view')
-_rz(z,hG,'class',0,e,s,gg)
-var oH=_oz(z,1,e,s,gg)
-_(hG,oH)
-_(r,hG)
+var tM=_n('view')
+_rz(z,tM,'class',0,e,s,gg)
+var eN=_oz(z,1,e,s,gg)
+_(tM,eN)
+_(r,tM)
 return r
 }
 e_[x[1]]={f:m1,j:[],i:[],ti:[],ic:[]}
@@ -1229,7 +1247,7 @@ setCssToHead([])();setCssToHead([[2,0]],undefined,{path:"./app.wxss"})();
 __wxAppCode__['app.wxss']=setCssToHead([[2,0]],undefined,{path:"./app.wxss"});    
 __wxAppCode__['app.wxml']=$gwx('./app.wxml');
 
-__wxAppCode__['pages/index/index.wxss']=setCssToHead([".",[1],"content { text-align: center; height: ",[0,400],"; }\n.",[1],"logo{ height: ",[0,200],"; width: ",[0,200],"; margin-top: ",[0,200],"; }\n.",[1],"title { font-size: ",[0,36],"; color: #8f8f94; }\n",],undefined,{path:"./pages/index/index.wxss"});    
+__wxAppCode__['pages/index/index.wxss']=setCssToHead(["@charset \x22UTF-8\x22;\n.",[1],"index .",[1],"swiper { width: 100%; height: ",[0,300],"; }\n.",[1],"index .",[1],"swiper .",[1],"swiper-item { display: -webkit-box; display: -webkit-flex; display: flex; -webkit-box-pack: center; -webkit-justify-content: center; justify-content: center; -webkit-box-align: center; -webkit-align-items: center; align-items: center; }\n.",[1],"index .",[1],"swiper .",[1],"swiper-item .",[1],"image { width: 100%; height: ",[0,260],"; margin: 0 ",[0,20],"; }\n.",[1],"index .",[1],"swiper .",[1],"swiper-item .",[1],"active { -webkit-transform: scale(1.14); transform: scale(1.14); -webkit-transition: all 0.2s ease-in 0.2s; transition: all 0.2s ease-in 0.2s; }\n",],undefined,{path:"./pages/index/index.wxss"});    
 __wxAppCode__['pages/index/index.wxml']=$gwx('./pages/index/index.wxml');
 
 __wxAppCode__['pages/user/user.wxss']=undefined;    
